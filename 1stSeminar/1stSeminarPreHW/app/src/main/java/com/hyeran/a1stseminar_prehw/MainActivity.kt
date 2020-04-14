75e27ecba5f5b727b772c77336ae17e02496e4c6
HyeranShin <hyeran9712@naver.com>
2020-04-14 19:01:34 +0900

    add: 1차 세미나 사전과제

---
+++ 1stSeminar/1stSeminarPreHW/app/src/main/java/com/hyeran/a1stseminar_prehw/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hyeran.a1stseminar_prehw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
